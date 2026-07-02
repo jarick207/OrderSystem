d4e299b4a496d5cb9139eef3916abac79b9529ba
jarick207 <jarick207@gmail.com>
2026-07-03 00:52:08 +0800

    Delete docs directory

--- docs/config.js
+++
@@ -1,3 +0,0 @@
-// 本機測試時保持空字串，會使用同一台 server 的 /api。
-// 發布到 GitHub Pages 時，請改成 Google Apps Script Web App URL。
-window.ORDER_API_BASE_URL = "https://script.google.com/macros/s/AKfycbysW47jT961Ml9KJCZ_h653Z9Eh-g_R-QqcVFLFgL6qs1ZQ4qWIAwMf0xyXqXir3aSVUQ/exec";
